32cd6c9e5262359f3324b6c8cf13d33175aedbdb
AnimeSosiska <mikegolikov3838@gmail.com>
2023-01-13 15:59:10 +0300

    Added target and router

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-export default defineNuxtConfig({
+export default({
+  target: 'static',
+  router:{
+    base: '/fooder_static/'
+  },
   app:{
+    baseURL: "/fooder/",
     head:{
       title:'Fooder',
       meta:[
