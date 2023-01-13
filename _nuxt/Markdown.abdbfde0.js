2c0f85dd587b83e115b560e156d300ece9ad83fd
AnimeSosiska <mikegolikov3838@gmail.com>
2023-01-13 16:02:33 +0300

    1b3d817

---
+++ _nuxt/Markdown.abdbfde0.js
@@ -0,0 +1,1 @@
+import s from"./ContentSlot.14b2c647.js";import{e as o,f,p,A as u}from"./entry.acc5f8d3.js";import"./utils.969a5e4a.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=u(),{between:n,default:a}=f(),r=p(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
---
+++ _nuxt/index.5e2504e6.js
@@ -0,0 +1,1 @@
+import{J as e}from"./entry.acc5f8d3.js";const n={};function r(c,t){return null}const o=e(n,[["render",r]]);export{o as default};
